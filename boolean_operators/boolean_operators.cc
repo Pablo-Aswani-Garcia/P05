1edbe0d96457b104b07827c735697c8f6c3a8be1
Pablo-Aswani-Garcia <alu0101546001@ull.edu.es>
2022-10-31 14:14:11 +0000

    last commit

--- boolean_operators/boolean_operators.cc
+++ boolean_operators/boolean_operators.cc
@@ -12,30 +12,21 @@
   * @see https://github.com/IB-2022-2023/P05-expressions/blob/main/expressions.md
   */
 
-
-
 #include <iostream>
 
-
 int main() {
-  bool bool_a{false}, bool_b{false};
-
-  for(int i{0}; i < 2; i++) {
-    for(int j{0}; j < 2; j++) {
-      std::cout << "bool a is: " << bool_a << " bool b is: " << bool_b <<
+  for(int i{0}; i <= true; ++i) {
+    for(int j{0}; j <= true; ++j) {
+      std::cout << "bool a is: " << i << " bool b is: " << j <<
                    std::endl;
-      std::cout << "AND door: " << (bool_a && bool_b) << std::endl;
-      std::cout << "OR door: " << (bool_a || bool_b) << std::endl;
-      std::cout << "NOT (A) door: " << (!bool_a) << std::endl;
-      std::cout << "NOT (B) door: " << (!bool_b) << std::endl;
-       std::cout << "NOR door: " << !(bool_a || bool_b) << std::endl;
-      std::cout << "NAND door: " << !(bool_a && bool_b) << std::endl;
-      std::cout << "XOR door: " << ((bool_a || bool_b) && !(bool_a && bool_b)) << std::endl;
-      bool_b=true;
+      std::cout << "AND: " << (i && j) << std::endl;
+      std::cout << "OR: " << (i || j) << std::endl;
+      std::cout << "NOT (A): " << (!i) << std::endl;
+      std::cout << "NOT (B): " << (!j) << std::endl;
+       std::cout << "NOR: " << !(i || j) << std::endl;
+      std::cout << "NAND: " << !(i && j) << std::endl;
+      std::cout << "XOR: " << ((i || j) && !(i && j)) << std::endl;
     }
-
-    bool_b=false;
-    bool_a=true;
   }
   return 0;
 }
